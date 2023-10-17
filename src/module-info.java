b5d55485bdce1e6fe28e5c524c6e5849aaa7c2f9
Urtzi <urcrmedev@gmail.com>
2023-10-17 13:20:37 +0200

    ADD Added java connector and ava.ql to module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -9,6 +9,8 @@ module EjercicioH {
 	requires javafx.fxml;
 	requires javafx.controls;
 	requires javafx.base;
+	requires mysql.connector.j;
+	requires java.sql;
 	opens tablaPersona to javafx.graphics, javafx.fxml;
 	opens model to javafx.fxml, javafx.base;
 	opens controllers to javafx.fxml, javafx.base;
